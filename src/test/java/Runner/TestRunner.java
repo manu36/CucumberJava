b055fbe6b58a5a607800470d615261146d34b234
manu36 <bakan11@gmail.com>
2021-10-18 14:31:13 -0300

    Se modifica archivo runn

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,7 +5,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"})
+@CucumberOptions(
+		features="src/test/resources/Features", 
+		glue= {"StepDefinitions"},
+		monochrome=true
+		)
 
 
 public class TestRunner {
